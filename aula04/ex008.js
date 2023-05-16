225f8f870d50ca10d724ad83c8ede758557b1221
Caroliny Rocha Sampaio <129858370+Carolinyr9@users.noreply.github.com>
2023-05-16 18:04:39 -0300

    m

---
+++ aula04/ex008.js
@@ -0,0 +1,7 @@
+var vel = 9.5
+console.log(`A velocida do seu carro é ${vel} km/h`)
+
+if (vel > 60){ //Condição simples
+    console.log("Você ultrapassou a velocidade permitida, MULTADO!")
+}
+console.log('Dirija usando cinto de segurança!')
